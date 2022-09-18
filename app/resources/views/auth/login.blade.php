4a02ca105b2aebe103977389505c736f6b182ae1
will3452 <williamgalas2@gmail.com>
2022-09-19 00:09:37 +0800

    initial setup of template

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -1,73 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="row justify-content-center">
+    <div class="col-lg-8">
+      <div class="card-group d-block d-md-flex row">
+        <div class="card col-md-7 p-4 mb-0">
+          <form class="card-body" action="/login" method="POST">
+            @csrf
+            <h1>Login</h1>
+            <p class="text-medium-emphasis">Sign In to your account</p>
+            <div class="input-group mb-3"><span class="input-group-text">
+                <svg class="icon">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                </svg></span>
+              <input class="form-control" type="text" name="email" placeholder="Username">
             </div>
+            <div class="input-group mb-4"><span class="input-group-text">
+                <svg class="icon">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                </svg></span>
+              <input class="form-control" name="password" type="password" placeholder="Password">
+            </div>
+            <div class="row">
+              <div class="col-6">
+                <button class="btn btn-primary px-4" type="submit">Login</button>
+              </div>
+              <div class="col-6 text-end">
+                <a class="btn btn-link px-0" href="/forgot-password">Forgot password?</a>
+              </div>
+            </div>
+          </form>
         </div>
+        <div class="card col-md-5 text-white bg-primary py-5">
+          <div class="card-body text-center">
+            <div>
+              <h2>Sign up</h2>
+              <p>Explore our app.</p>
+              <a class="btn btn-lg btn-outline-light mt-3" href="/register">Register Now!</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
