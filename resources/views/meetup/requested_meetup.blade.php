54221a70f67c98786dbb1725c4ea4edea6d5624c
will3452 <williamgalas2@gmail.com>
2022-10-05 22:08:37 +0800

    wip

--- resources/views/meetup/requested_meetup.blade.php
+++ resources/views/meetup/requested_meetup.blade.php
@@ -8,11 +8,6 @@
             'active' => false,
         ],
         [
-            'label' => 'Requesting Meetup',
-            'link' => '/meetup/request-meetup',
-            'active' => false
-        ],
-        [
             'label' => 'Requested Meetup',
             'link' => '/meetup/requested-meetup',
             'active' => True
@@ -20,44 +15,31 @@
     ]"/>
 @endsection
 @section('content')
-        <div class="card mt-2">
-            <div class="card-header">
-                <div>Requested Meetup</div>
-            </div>
-            <div class="card-body table_auto">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                          <th scope="col" class="text-truncate">Remarks</th>
-                          <th scope="col" class="text-truncate">Requested</th>
-                          <th scope="col" class="text-truncate">Post</th>
-                          <th scope="col" class="text-truncate">Date Created</th>
-                          <th scope="col" class="text-truncate">Approved At</th>
-                          <th scope="col" class="text-truncate">Declined At</th>
-                          {{-- <th scope="col"></th> --}}
-                        </tr>
-                      </thead>
-                      <tbody>
-                      
-                          @foreach ($meetupdata as $data)
-                              
-                          <tr>
-                              <td class="text-truncate">{{$data->remarks}}</td>
-                              <td class="text-truncate"><a href="">{{ucwords($data->getUserRequested()->name)}}</a></td>
-                              <td class="text-truncate"><a href="">{{$data->getPost()->title}}</a></td>
-                              <td class="text-truncate">{{$data->created_at->diffForHumans()}}</td>
-                              <td class="text-truncate">{{$data->approved_at ?: '-----'}}</td>
-                              <td class="text-truncate">{{$data->declined_at ?: '-----'}} </td>
-                              {{-- <td class="text-truncate">
-                                  <div class="d-flex justify-content-center">
-                                      <a href="/meetup/request-meetup/{{$data->id}}/process" class="btn btn-outline-primary m-1">Process</a>
-                                  </div>
-                              </td> --}}
-                          </tr>
-                          @endforeach
-                      </tbody>
-                </table>
-                {{ $meetupdata->links("pagination::bootstrap-4") }}
-            </div>
-        </div>
+        <h1>My Request</h1>
+        <div class="list-group">
+            @forelse ($meetupdata as $data)
+                <a class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{$data->getPost()->title}}
+                        @if (! is_null($data->approved_at))
+                            <span class="badge badge-sm bg-success" >Approved</span>
+                        @endif
+
+                        @if (! is_null($data->declined_at))
+                            <span class="badge badge-sm bg-danger" >Declined</span>
+                        @endif
+                    </h5>
+                    <small class="text-muted">{{$data->created_at->diffForHumans()}}</small>
+                </div>
+                <p class="mb-1">{{ucwords($data->getUserRequested()->name)}}</p>
+                <small class="text-muted">{{$data->remarks}}</small>
+                </a>
+            @empty
+            <a class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">No data found.</h5>
+                </div>
+                </a>
+            @endforelse
+          </div>
 @endsection
